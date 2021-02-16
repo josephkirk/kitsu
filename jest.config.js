62edd82b7292d3e28070309aa5f2052f5c988eff
Nguyen Phi Hung <josephkirk.art@gmail.com>
2021-02-16 13:25:46 +0700

    update from origin

--- jest.config.js
+++ jest.config.js
@@ -15,34 +15,10 @@ module.exports = {
   ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     "js-3d-model-viewer": "<rootDir>/tests/substituted-model-viewer.js",
     "vue-slider-component": "<rootDir>/tests/substituted-model-viewer.js",
     "fabric": "<rootDir>/tests/fabric.js",
     "vue-loading-spinner": "<rootDir>/tests/spinner.js"
-=======
-    'js-3d-model-viewer': '<rootDir>/tests/substituted-model-viewer.js',
-    'vue-slider-component': '<rootDir>/tests/substituted-model-viewer.js',
-    fabric: '<rootDir>/tests/fabric.js'
->>>>>>> caf16d6c (Add electron)
-=======
-    "js-3d-model-viewer": "<rootDir>/tests/substituted-model-viewer.js",
-    "vue-slider-component": "<rootDir>/tests/substituted-model-viewer.js",
-    "fabric": "<rootDir>/tests/fabric.js"
->>>>>>> 1af13f21 (Revert "Add electron")
-=======
-    'js-3d-model-viewer': '<rootDir>/tests/substituted-model-viewer.js',
-    'vue-slider-component': '<rootDir>/tests/substituted-model-viewer.js',
-    fabric: '<rootDir>/tests/fabric.js'
->>>>>>> f4faebb5 (Add electron)
-=======
-    "js-3d-model-viewer": "<rootDir>/tests/substituted-model-viewer.js",
-    "vue-slider-component": "<rootDir>/tests/substituted-model-viewer.js",
-    "fabric": "<rootDir>/tests/fabric.js"
->>>>>>> ee7fffeb (Revert "Add electron")
   },
   setupFiles: ['<rootDir>/tests/setup.js'],
   snapshotSerializers: [
